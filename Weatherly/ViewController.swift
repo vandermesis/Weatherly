a05236dc30dbc735ff86cf282aabf0daf42f1573
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2019-08-23 14:22:17 +0200

    Initial Commit

---
+++ Weatherly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weatherly
+//
+//  Created by Marek Skrzelowski on 23/08/2019.
+//  Copyright © 2019 vandermesis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
