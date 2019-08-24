1ea270db6388bb7d728510310f9491addea9809b
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2019-08-24 22:52:45 +0200

    Get current location using CoreLocation

--- Weatherly/Controller/NowViewController.swift
+++ Weatherly/Controller/NowViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import Alamofire
+import ChameleonFramework
+import SwiftyJSON
+import CoreLocation
 
-class NowViewController: UIViewController {
+class NowViewController: UIViewController, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Start updating location data
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.startUpdatingLocation()
     }
 
-
+    // MARK: - Constants and Variables
+    // Constants:
+    let darkSkyURL = "https://api.darksky.net/forecast/866e7c9ce3ac206ce04b8644068470c5/"
+    let locationManager = CLLocationManager()
+    
+    // Variables:
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    //MARK: - Get current location data
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let currentLocation = locations[locations.count - 1]
+        if currentLocation.horizontalAccuracy > 0 {
+            // Stop updating location data
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            let latitude = currentLocation.coordinate.latitude
+            let longitude = currentLocation.coordinate.longitude
+            let locationCoordinates = "\(latitude),\(longitude)"
+            print(locationCoordinates)
+            getWeatherData(url: darkSkyURL, location: locationCoordinates)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        cityLabel.text = "Location Unavaiable"
+    }
+    
+    func getWeatherData(url: String, location: String) {
+        
+    }
 }
 
