37c2c58929029820411944767836cfa62a806f5b
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2019-08-26 00:48:32 +0200

    Create custom FutureCell and update FutureViewController with forecast data

--- Weatherly/Controller/FutureTableViewController.swift
+++ Weatherly/Controller/FutureTableViewController.swift
@@ -9,75 +9,39 @@
 import UIKit
 import SwiftSky
 
-class FutureTableViewController: UITableViewController {
+class FutureTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        print(weatherDataForecast ?? "error")
+        futureTableView.delegate = self
+        futureTableView.dataSource = self
+        futureTableView.separatorStyle = .none
+        futureTableView.register(UINib(nibName: "FutureCell", bundle: nil), forCellReuseIdentifier: "futureCell")
     }
+    
     
     // MARK: - Variables
     var weatherDataForecast: [DataPoint]?
-
+    @IBOutlet weak var futureTableView: UITableView!
+    
+    let dateFormatter = DateFormatter()
+    
+    
     // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return weatherDataForecast?.count ?? 1
     }
 
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ForecastCell", for: indexPath)
-        let maxTemp = weatherDataForecast![indexPath.row].temperature?.max?.value
-        cell.textLabel?.text = String(maxTemp ?? 00)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "futureCell", for: indexPath) as! FutureCell
+        cell.dayMaxTemp.text = String(Int(round(weatherDataForecast![indexPath.row].temperature?.max?.value ?? 99)))
+        cell.dayMinTemp.text = String(Int(round(weatherDataForecast![indexPath.row].temperature?.min?.value ?? 99)))
+        cell.dayOfWeek.text = dateFormatter.weekdaySymbols[Calendar.current.component(.weekday, from: weatherDataForecast![indexPath.row].time) - 1]
+        cell.dayWeatherIcon.image = UIImage(named: weatherDataForecast![indexPath.row].icon!)
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    @IBAction func nowButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ Weatherly/View/FutureCell/FutureCell.swift
@@ -0,0 +1,21 @@
+//
+//  FutureCell.swift
+//  Weatherly
+//
+//  Created by Marek Skrzelowski on 26/08/2019.
+//  Copyright © 2019 vandermesis. All rights reserved.
+//
+
+import UIKit
+
+class FutureCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    @IBOutlet weak var dayOfWeek: UILabel!
+    @IBOutlet weak var dayMaxTemp: UILabel!
+    @IBOutlet weak var dayMinTemp: UILabel!
+    @IBOutlet weak var dayWeatherIcon: UIImageView!
+}
