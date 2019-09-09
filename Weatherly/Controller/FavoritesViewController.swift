3ed18d9a1d7a743b336e8a3c2bd75077907e1bd2
Marek Skrzelowski <mskrzelowski@vandermesis.com>
2019-09-09 12:45:47 +0200

    Add favorites viewcontroller, remote ChameleonFramework pod.

---
+++ Weatherly/Controller/FavoritesViewController.swift
@@ -0,0 +1,43 @@
+//
+//  FavoritesViewController.swift
+//  Weatherly
+//
+//  Created by Marek Skrzelowski on 02/09/2019.
+//  Copyright © 2019 vandermesis. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension FavoritesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
